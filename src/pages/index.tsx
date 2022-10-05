2fcc4a3c0430b0e07acaaea72b65a3fc07a1a7f7
iritep <rpat.upwork@gmail.com>
2022-10-05 16:23:08 -0500

    feat: login router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Button, Box } from '@mui/material';
+import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Home Page</div>;
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push('/login');
+  };
+  return (
+    <div>
+      <Button onClick={handleLogin}>Login</Button>
+      <Box>Home Page</Box>
+    </div>
+  );
 };
 
 export default Home;

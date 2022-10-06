467e7fd37236a8be119b4a12e63ed1dc1f5238f7
iritep <rpat.upwork@gmail.com>
2022-10-05 22:52:02 -0500

    fix: added the env generate script

--- next.config.js
+++ next.config.js
@@ -8,15 +8,7 @@ const { parsed: appEnv } = require('dotenv').config({
 
 module.exports = withImages({
   webpack(config) {
-    if (!appEnv) {
-      config.plugins.push(
-        new webpack.EnvironmentPlugin({
-          NODE_ENV: process.env.NODE_ENV,
-          API_URL: process.env.API_URL,
-          HOSTING_URL: process.env.HOSTING_URL,
-        })
-      );
-    } else config.plugins.push(new webpack.EnvironmentPlugin(appEnv));
+    config.plugins.push(new webpack.EnvironmentPlugin(appEnv));
     return config;
   },
   images: {

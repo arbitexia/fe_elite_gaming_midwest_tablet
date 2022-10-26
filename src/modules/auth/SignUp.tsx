41158d88688e4b72818958ee53ec20202fb83daa
iritep <rpat.upwork@gmail.com>
2022-10-25 22:08:27 -0500

    fix: build error

--- src/modules/auth/SignUp.tsx
+++ src/modules/auth/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, ReactNode } from 'react';
 import { Box, Checkbox, Typography, Stack } from '@mui/material';
 import {
   UIAuthTabs,
@@ -12,7 +12,7 @@ import { useFormik } from 'formik';
 import { useAppToast } from '@/providers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { MobileDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import InputMask from 'react-input-mask';
+import InputMask, { Props, ReactInputMask } from 'react-input-mask';
 import { Moment } from 'moment';
 
 export const SignUp = () => {
@@ -90,15 +90,12 @@ export const SignUp = () => {
                 formik.handleChange(e);
               }}
               disabled={false}
-              maskChar="X"
+              maskPlaceholder="X"
             >
-              {(inputProps: any) => (
-                <UIDefaultTextField
-                  {...inputProps}
-                  name="phoneNumber"
-                  placeholder="Phone Number"
-                />
-              )}
+              <UIDefaultTextField
+                name="phoneNumber"
+                placeholder="Phone Number"
+              />
             </InputMask>
 
             <UIDefaultTextField

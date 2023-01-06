b34b4bcde5f3a289c647fe8adfb99e473cd34dfe
iritep <rpat.upwork@gmail.com>
2023-01-06 16:39:13 -0600

    test: deploy redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,12 @@ import { SignUp, CheckIn } from '@/modules/auth';
 const HomePage: NextPage = () => {
   const router = useRouter();
   const { path: pageType } = router.query;
-  if (router.pathname === '/') {
-    return (
-      <AuthLayout title={pageType === 'signup' ? 'Sign Up' : 'Check In'}>
-        {pageType === 'signup' ? <SignUp /> : <CheckIn />}
-      </AuthLayout>
-    );
-  } else {
-    router.replace(router.asPath);
-    return <></>;
-  }
+
+  return (
+    <AuthLayout title={pageType === 'signup' ? 'Sign Up' : 'Check In'}>
+      {pageType === 'signup' ? <SignUp /> : <CheckIn />}
+    </AuthLayout>
+  );
 };
 
 export default HomePage;

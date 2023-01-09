c6d1739761282630da0b4dfcf4bb45f8d4c8483f
iritep <rpat.upwork@gmail.com>
2023-01-09 00:39:19 -0600

    fix: rewards wrap panel

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -8,6 +8,7 @@ export {
 export { UIContainer } from './Container';
 export { UIImage } from './Image';
 export { UIAppLayoutWrapper } from './Wrapper';
+export { UIWrapPanel } from './WrapPanel';
 export { UIDefaultTextField, UISearchBox, UISelectBox } from './TextField';
 export { UIDefaultButton, UIBackButton, UINumberButton } from './Button';
 export { UIAuthTabs } from './Tab';

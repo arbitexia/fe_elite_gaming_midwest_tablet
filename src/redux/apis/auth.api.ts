0d50c15d96e550c19a39c7a8cbebb0cfa70f2447
iritep <rpat.upwork@gmail.com>
2023-01-09 15:42:41 -0600

    fix: api failed

--- src/redux/apis/auth.api.ts
+++ src/redux/apis/auth.api.ts
@@ -46,7 +46,7 @@ export const authorizeTablet = async (params: TabletAuthParams) => {
 
 export const authorizeCustomer = async (params: CustomerAuthParams) => {
   const response = await axios.post(
-    `${baseUrl}/api/authorizeCustomer`,
+    `${baseUrl}/tablet/authorizeCustomer`,
     params,
     {
       headers: authHeader,
@@ -56,14 +56,14 @@ export const authorizeCustomer = async (params: CustomerAuthParams) => {
 };
 
 export const register = async (params: RegisterParams) => {
-  const response = await axios.post(`${baseUrl}/api/register`, params, {
+  const response = await axios.post(`${baseUrl}/tablet/register`, params, {
     headers: authHeader,
   });
   return response.data;
 };
 
 export const verifyPhone = async (params: VerifyPhoneParams) => {
-  const response = await axios.post(`${baseUrl}/api/verifyPhone`, params, {
+  const response = await axios.post(`${baseUrl}/tablet/verifyPhone`, params, {
     headers: authHeader,
   });
   return response.data;
